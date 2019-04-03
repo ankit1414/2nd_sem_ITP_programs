601df09f2a1f9e879431074ec8486a5d8dbdf182
ANKIT <45892437+ankit1414@users.noreply.github.com>
2019-04-03 19:44:23 +0530

    Add files via upload

---
+++ swaping_of_two_numbers_using_variable.c
@@ -0,0 +1,15 @@
+//DEVELOPED BY: ANKIT BHARDWAJ
+#include<stdio.h>
+void main(){
+    int n1,n2,temp;
+    printf("Enter n1:::  ");
+    scanf("%d",&n1);
+    printf("Enter n2:::  ");
+    scanf("%d",&n2);
+    temp=n1;
+    n1=n2;
+    n2=temp;
+    printf("\nSwapped Values::\nn1=%d   n2=%d\n",n1,n2);
+    printf("\n\t\t\t\t\tANKIT(D-6)\n");
+
+}
