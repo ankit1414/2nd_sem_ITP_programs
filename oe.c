b1bf749b0da242f8d5efe3398b9c24a7b500ea57
ANKIT <45892437+ankit1414@users.noreply.github.com>
2019-04-03 20:23:21 +0530

    Add files via upload

---
+++ oe.c
@@ -0,0 +1,15 @@
+//DEVELOPED BY: ANKIT BHARDWAJ
+#include<stdio.h>
+int main()
+{	int a;
+	printf("ENTER THE NUMBER::: ");
+	scanf("%d",&a);
+	if(a%2==0)
+		printf("\nTHE NUMBER IS EVEN\n");
+	else
+		printf("\nTHE NUMBER IS ODD\n");
+    printf("\n\t\t\t\t\tANKIT(D-6)\n");
+	return 0;
+}
+
+
