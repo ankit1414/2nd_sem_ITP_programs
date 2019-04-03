bb09e76039b35c82f2d5f4a7d2f89548c5394c08
ANKIT <45892437+ankit1414@users.noreply.github.com>
2019-04-03 20:39:15 +0530

    Add files via upload

---
+++ pattern_star.c
@@ -0,0 +1,15 @@
+//DEVELOPED BY: ANKIT BHARDWAJ
+#include<stdio.h>
+int main()
+{
+    for(int i=1;i<6;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            printf(" * ");
+        }
+        printf("\n");
+    }
+    printf("\n\t\t\t\t\tANKIT(D-6)\n");
+    return 0;
+}
