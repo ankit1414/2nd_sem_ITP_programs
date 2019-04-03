a498c4106faf7bb840860c0ed69a2baa21fcf94b
ANKIT <45892437+ankit1414@users.noreply.github.com>
2019-04-03 20:28:32 +0530

    Add files via upload

---
+++ ASCII_VALUE.c
@@ -0,0 +1,11 @@
+//DEVELOPED BY: ANKIT BHARDWAJ
+#include<stdio.h>
+int main()
+{
+    char CH;
+    printf("Enter the number : ");
+    scanf("%c",&CH);
+    printf("Ascii code is : %d",CH);
+    printf("\n\t\t\t\t\tANKIT(D-6)\n");
+    return 0;
+}
