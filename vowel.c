f900b4b280a23fa289b2ed102cd52a6de8efe710
ANKIT <45892437+ankit1414@users.noreply.github.com>
2019-04-03 23:21:03 +0530

    Add files via upload

---
+++ vowel.c
@@ -0,0 +1,14 @@
+//DEVELOPED BY: ANKIT BHARDWAJ
+#include<stdio.h>
+void main()
+{
+    char ch;
+    printf("Enter the character : ");
+    scanf("%c",&ch);
+    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+        printf("\nCharacter is vowel!!!");
+    else
+        printf("\nCharacter is NOT a vowel!!!");
+    printf("\n\t\t\t\t\tANKIT(D-6)\n");
+
+}
