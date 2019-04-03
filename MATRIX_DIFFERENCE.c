bae50f977b84f90178373ccaf8385bfe23505ba9
ANKIT <45892437+ankit1414@users.noreply.github.com>
2019-04-03 23:19:14 +0530

    Add files via upload

---
+++ MATRIX_DIFFERENCE.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+void main()
+{
+    int r,c;
+    int arr1[20][20],arr2[20][20],arrdif[20][20];
+    printf("\nEnter the number of rows and columns: ");
+    scanf("%d %d",&r,&c);
+    printf("\nEnter the elements of 1st matrix:\n");
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++)
+            scanf("%d",&arr1[i][j]);
+    }
+    printf("\nEnter the elements of 2nd matrix:\n");
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++)
+            scanf("%d",&arr2[i][j]);
+    }
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++)
+            arrdif[i][j]=arr1[i][j]-arr2[i][j];
+    }
+    printf("\nDifference matrix is:\n");
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++)
+            printf("%d ",arrdif[i][j]);
+	printf("\n");
+    }
+    printf("\n\t\t\t\t\tANKIT(D-6)\n");
+
+}
