76e59c977b7d7dc40dc2a91985d697ebe19bc32f
ANKIT <45892437+ankit1414@users.noreply.github.com>
2019-04-03 20:05:13 +0530

    Add files via upload

---
+++ ARITHMATIC_OPERATIONS.c
@@ -0,0 +1,18 @@
+//DEVELOPED BY: ANKIT BHARDWAJ
+#include<stdio.h>
+void main(){
+
+    int n1,n2;
+    printf("Enter n1  :: ");
+    scanf("%d",&n1);
+    printf("Enter n2  :: ");
+    scanf("%d",&n2);
+    printf("ARITHMATIC OPERATIONS::\n");
+    printf("Addition       :: %d\n",n1+n2);
+    printf("Subtraction    :: %d\n",n1-n2);
+    printf("Multiplication :: %d\n",n1*n2);
+    printf("Division       :: %f\n",(n1*1.0)/n2);
+    printf("Remainder      :: %d\n",n1%n2);
+    printf("\n\t\t\t\t\tANKIT(D-6)\n");
+
+}
