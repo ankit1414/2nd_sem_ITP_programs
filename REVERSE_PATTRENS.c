e1c7c1704a096645cb6c563200e9f0057dc4552f
ANKIT <45892437+ankit1414@users.noreply.github.com>
2019-04-03 21:12:53 +0530

    Add files via upload

---
+++ REVERSE_PATTRENS.c
@@ -0,0 +1,18 @@
+//DEVELOPED BY: ANKIT BHARDWAJ
+#include<stdio.h>
+void main(){
+    for(int i=5;i>=1;i--){
+        for(int j=1;j<=i;j++){
+            printf("%d ",i);
+        }
+        printf("\n");
+    }
+    printf("\n\n");
+    for(int i=5;i>=1;i--){
+        for(int j=1;j<=i;j++){
+            printf("%d ",j);
+        }
+        printf("\n");
+    }
+    printf("\n\t\t\t\t\tANKIT(D-6)\n");
+}
