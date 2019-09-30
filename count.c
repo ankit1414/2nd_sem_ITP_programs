d40b2d9370fad1cc124493ab881c460557775142
Deepak <deepak.mca19.du@gmail.com>
2019-10-01 00:25:56 +0530

    Update count.c

--- count.c
+++ count.c
@@ -11,9 +11,9 @@ int main()
     for(i=0; i<siz; ++i)
     {   if(arr[i]>=0)
             cp++;
-        if(arr[i]<0)
+        else if(arr[i]<0)
             cn++;
-        if(arr[i]%2==0)
+        else
             ce++;
     }
     printf("The number of positive integers in the array == %d",cp);
